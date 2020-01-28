985455cf95fbb55bf6b2132c56aa0558dc84b4bb
shantanuchandalia <shantanu.chandalia@gmail.com>
2020-01-28 13:10:48 +0530

    1. Removed unused imports 2. Added a Sanity testng.xml

--- src/main/java/com/testAutomationTest/QA/pages/MyProfile.java
+++ src/main/java/com/testAutomationTest/QA/pages/MyProfile.java
@@ -1,7 +1,5 @@
 package com.testAutomationTest.QA.pages;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.testAutomationTest.QA.base.TestBase;
