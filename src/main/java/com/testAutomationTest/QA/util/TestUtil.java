e3adba7909356eb6d0bac068c2732173852cf799
shantanuchandalia <shantanu.chandalia@gmail.com>
2020-01-29 07:45:21 +0530

    Basic utils added
    1. Added WebDriver Event Listener
    2. Screenshot
    3. POI generic function to read data from excel

--- src/main/java/com/testAutomationTest/QA/util/TestUtil.java
+++ src/main/java/com/testAutomationTest/QA/util/TestUtil.java
@@ -1,13 +1,60 @@
 package com.testAutomationTest.QA.util;
 
+import java.io.*;
+import java.io.IOException;
+
+import org.apache.commons.compress.archivers.dump.InvalidFormatException;
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 import com.testAutomationTest.QA.base.TestBase;
 
-public class TestUtil extends TestBase{
+public class TestUtil extends TestBase {
+	private static final String TESTDATA_SHEET_PATH = null;
 	public static long PAGE_LOAD_TIMEOUT = 20;
 	public static long IMPLICIT_WAIT = 10;
-	
-	public void switchToFrame()
-	{
+	static Workbook book;
+	static Sheet sheet;
+
+	public void switchToFrame() {
 		driver.switchTo().frame("");
+	}
+
+	public static Object[][] getTestData(String sheetName) {
+		FileInputStream file = null;
+		try {
+			file = new FileInputStream(TESTDATA_SHEET_PATH);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			book = WorkbookFactory.create(file);
+		} catch (InvalidFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		sheet = book.getSheet(sheetName);
+		Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
+		// System.out.println(sheet.getLastRowNum() + "--------" +
+		// sheet.getRow(0).getLastCellNum());
+		for (int i = 0; i < sheet.getLastRowNum(); i++) {
+			for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) {
+				data[i][k] = sheet.getRow(i + 1).getCell(k).toString();
+				// System.out.println(data[i][k]);
+			}
+		}
+		return data;
+	}
+
+	public static void takeScreenshotAtEndOfTest() throws IOException {
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String currentDir = System.getProperty("user.dir");
+		FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
+
 	}
 }
